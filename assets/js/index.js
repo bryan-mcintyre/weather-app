ea937d6595682660e6c03ce820db34086099b4ab
bryan-mcintyre <167257103+bryan-mcintyre@users.noreply.github.com>
2024-06-06 18:25:30 -0500

    Updated js as of 06/06/24 18:25

--- assets/js/index.js
+++ assets/js/index.js
@@ -6,24 +6,41 @@ const cityInput = document.querySelector(".cityInput")
 const card = document.querySelector(".card")
 const apiKey = "d1f494054696aa2d95d7b146eb42db19"
 
-// Form submit event listener
+// Form submit event listener and API fetch request with error for invalid form submission
 weatherForm.addEventListener("click", function(event) {
     event.preventDefault()
 
     const city = cityInput.value
 
     if(city) {
+        fetch(`http://api.openweathermap.org/geo/1.0/direct?q=${city}&appid=${apiKey}`)
+            .then(function(response) {
+                return response.json()
+            })
+            .then(function (data) {
+                console.log(data)
+                for (let i = 0; i < data.length; i++) {
+                    const lat = document.createElement("p")
+                    const lon = document.createElement("p")
+                    card.textContent = ""
+                    lat.textContent = data[i].lat
+                    lon.textContent = data[i].lon
+                    card.appendChild(lat)
+                    card.appendChild(lon)
+                    localStorage.setItem("lat", data[i].lat)
+                    localStorage.setItem("lon", data[i].lon)
+                }
 
+                // localStorage.setitem("City")
+            })
     } else {
         displayError()
-    }
-})
-
-// API fetch request
-fetch()
-
+        }
+        })
+        
 // Display weather info
 function displayWeatherInfo() {
+    
 
 }
 
