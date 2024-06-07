1bc794fb29868fbd0782c377573e89d3c63d29a4
bryan-mcintyre <167257103+bryan-mcintyre@users.noreply.github.com>
2024-06-07 17:23:38 -0500

    Got fetch requests and local storage working

--- assets/js/index.js
+++ assets/js/index.js
@@ -5,6 +5,7 @@ const weatherForm = document.querySelector(".weatherForm")
 const cityInput = document.querySelector(".cityInput")
 const card = document.querySelector(".card")
 const apiKey = "d1f494054696aa2d95d7b146eb42db19"
+let cityData = []
 
 // Form submit event listener and API fetch request with error for invalid form submission
 weatherForm.addEventListener("click", function(event) {
@@ -19,30 +20,54 @@ weatherForm.addEventListener("click", function(event) {
             })
             .then(function (data) {
                 console.log(data)
-                for (let i = 0; i < data.length; i++) {
-                    const lat = document.createElement("p")
-                    const lon = document.createElement("p")
-                    card.textContent = ""
-                    lat.textContent = data[i].lat
-                    lon.textContent = data[i].lon
-                    card.appendChild(lat)
-                    card.appendChild(lon)
-                    localStorage.setItem("lat", data[i].lat)
-                    localStorage.setItem("lon", data[i].lon)
-                }
+                localStorage.setItem("City-Data", JSON.stringify(data))
 
-                // localStorage.setitem("City")
+                const cityLat = data[0].lat
+                const cityLon = data[0].lon
+
+                fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${cityLat}&lon=${cityLon}&appid=${apiKey}&units=imperial`)
+                    .then(function(response) {
+                        return response.json()
+                    })
+                    .then(function(data) {
+                        console.log(data)
+                        localStorage.setItem("Weather", JSON.stringify(data))
+                        const temp = document.createElement("p")
+                        const humidity = document.createElement("p")
+
+                        card.textContent = ""
+
+                        temp.textContent = data[0].temp
+                        humidity.textContent = data[0].humidity
+                        card.appendChild(temp)
+                        card.appendChild(humidity)
+
+                        for (let i = 0; i < data.length; i++) {
+                            const temp = document.createElement("p")
+                            const humidity = document.createElement("p")
+                            card.textContent = ""
+                            temp.textContent = data[i].main.temp
+                            humidity.textContent = data[i].main.humidity
+                            card.appendChild(temp)
+                            card.appendChild(humidity)
+                    }})
             })
     } else {
         displayError()
         }
-        })
+
+    
+})
+
+
         
 // Display weather info
 function displayWeatherInfo() {
     
+        
+                    
+    }
 
-}
 
 // Current conditions emojis
 function weatherEmoji() {
